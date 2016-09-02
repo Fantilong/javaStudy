f873759ba1b42a8db6f357df9b0c3ed675cb10de
fantilong <2554509132@qq.com>
2016-09-02 11:29:31 +0800

    del

---
+++ javaXiTi/XiTi27.java
@@ -0,0 +1,9 @@
+public class XiTi27{
+	public static void main(String[] args){
+		System.out.print(2 + " ");
+                System.out.print(Math.sqrt(7) + " ");
+	//	for(int i = 3;i < 100;i += 2){
+		
+	//	}
+	}
+}
